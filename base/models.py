f967b9d9603821fc21c2930313eb557c7895e035
et860525 <et860525@gmail.com>
2021-04-06 20:24:30 +0800

    Send stock name and return correct data

--- base/models.py
+++ base/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-#class Stock(models.Model):
-#    name = 
-#    ticker = 
-#    price_portfolio 
-#    date = models.DateField()
-#    open = models.FloatField()
-#    high = models.FloatField()
-#    low = models.FloatField()
-#    close = models.FloatField()
-#    volume = models.IntegerField()
--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from base import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'result/<str:stockname>', views.search_result, name='stock_result')
 ]
