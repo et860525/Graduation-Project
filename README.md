ff83682e8ea9f217ea7d090b6b3515122429862e
et860525 <et860525@gmail.com>
2021-04-22 09:42:15 +0800

    Data visualization(NOT optimize), Front-end get Data, Add README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Stock Introduction Project
+This project makes what stock you search then you can see the **price visualization** and **stock information** on the website.
+
+## Installation
+* Python 3.8.x up
+* Pip 21.0.x up
+
+### Python Package
+`Suggest use virtualenv before install package`
+
+Use the package manager **pip** to install all.
+```bash
+pip install -r .\requirements.txt
+```
+
+## Usage
+```bash
+python manage.py runserver
+```
+`http://127.0.0.1:8000/` or `http://localhost:8000/`
+
+You can change host with yourself
--- base/views.py
+++ base/views.py
@@ -31,14 +31,51 @@ def search_result(request, stockname):
         #    print(pd.DataFrame(list(rs.items()),columns=['key','value']))
         
     if request.method == "GET":
-        final = yf.Ticker(stockname)    
+        final = yf.Ticker(stockname)   
+        stock_price_close = []
+        stock_date = []
         try:
-            info_result = pd.DataFrame(list(final.info.items()),columns=['key','value'])
-            print(info_result)
-            return render(request, 'base/stock_result.html', {'info_result': info_result})
+            ### Test Use DataFrame to get info ###
+            #info = pd.DataFrame.from_dict(final.info, orient='index').rename(columns={0: 'value'})
+
+            ### Check All Data ###
+            #info_result = pd.DataFrame(list(final.info.items()),columns=['key','value'])
+            #print(info_result)
+            
+            ### Get Specific INFO ###
+            infos = final.info
+            #print(infos)
+
+            info_result = {'shortName': infos['shortName'], 'country': infos['country'], 
+                    'city': infos['city'], 'address1': infos['address1'], 'industry': infos['industry'],
+                    'longBusinessSummary': infos['longBusinessSummary']}
+            #print(info_result)
+
+            ### Get History Data ###
+            stock_price = final.history(period='5y') # Get 10 year data
+            stock_price.reset_index(inplace=True) # Make "Date" become columns
+            
+            ### Currently need data ###
+            for date, close in zip(stock_price['Date'], stock_price['Close']):
+                stock_price_close.append(close) 
+                stock_date.append(str(date)[:10])
+            
+            ### TODO Add condition to check what data show ###
+            ### Show 5 YEARs ###
+            stock_date = [ ele[:9] for ele in stock_date ]
+            stock_date = sorted(list(set(stock_date)))
+            stock_5y_mean = []
+            
+            #print(stock_price['Date'])
+            for year in stock_date:
+                stock_5y_mean.append(stock_price[stock_price['Date'].astype(str).str.contains(year)]['Close'].mean())
+
+            print(stock_date)    
+            print(stock_5y_mean)
+
+            return render(request, 'base/stock_result.html', {'stockname': stockname, 'info_result': info_result
+                                                            , 'stock_price': stock_5y_mean, 'stock_date': stock_date})
         except ValueError as error:
             return HttpResponse('<h1>{} Not Found</h1>'.format(stockname))
-
-        return HttpResponse('<h1>We got {} GET</h1>'.format(stockname))
     return HttpResponse('<h1>Nothing</h1>')
     #return render(request, 'base/stock_result.html', {'stockname': stockname})
